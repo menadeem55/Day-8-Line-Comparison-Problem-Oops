cb54ee6b40fd7d15887260594a86068bee376898
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 17:40:38 +0530

    [Added: comparing line using Oops Concept ]

---
+++ src/com/blabz/java/LineCompressionMain.java
@@ -0,0 +1,13 @@
+package com.blabz.java;
+
+public class LineCompressionMain {
+	public static void main(String[] args) {
+		System.out.println("~~WELCOME TO LINE COMPARISON COMPUTATION PROGRAM~~");
+		LineComparison lc = new LineComparison();
+		lc.getLenght();
+		lc.compareLines();
+		
+	}
+
+
+}
